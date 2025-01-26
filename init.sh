385cd724e0cf2b14570d902989b479a257a3f155
Francesco <francescocirillo2000@gmail.com>
2025-01-26 19:50:36 +0100

    FIX: tun0 concurrency, added tls-auth

--- init.sh
+++ init.sh
@@ -3,9 +3,12 @@
 # initialize /dev/hwrng device for jail init (fails otherwise)
 sudo mknod /dev/hwrng c 1 8
 
-# initialize dev tun0 for openvpn no root mode
+# initialize dev tun0 tun1 for openvpn no root mode
 sudo my-experiments/build/openvpn/generator/openvpn --rmtun --dev tun0
 sudo my-experiments/build/openvpn/generator/openvpn --mktun --dev tun0 --dev-type tun --user user --group user
+
+sudo my-experiments/build/openvpn/generator/openvpn --rmtun --dev tun1
+sudo my-experiments/build/openvpn/generator/openvpn --mktun --dev tun1 --dev-type tun --user user --group user
 
 # create sudo wrapper for ip commands
 sudo sh -c 'echo "#!/bin/sh\nsudo /sbin/ip \$*" > /usr/local/sbin/unpriv-ip'
